cf6c32c7bc81f7946e519d8d8cf3d639dde8ec1e
Max <184944+montrealist@users.noreply.github.com>
2020-06-08 16:24:44 -0400

    gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,5 +3,54 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+const path = require(`path`)
+const { slash } = require(`gatsby-core-utils`)
 
-// You can delete this file if you're not using it
+const pageTemplateName = 'page.js';
+const pageTemplateRoot = './src/components';
+
+exports.createPages = async ({ graphql, actions }) => {
+    const { createPage, createRedirect } = actions
+
+    const rootPageSlug = '/miraculous-productions/'; // FIXME: have to hard-code this?
+
+    const pages = await graphql(`
+        query {
+            allWordpressPage(filter: {path: {glob: "${rootPageSlug}**"}}) {
+                edges {
+                    node {
+                        id
+                        path
+                        slug
+                        title
+                    }
+                }
+            }
+        }
+    `).then(result => result.data.allWordpressPage);
+
+    if (pages.errors) throw new Error(pages.errors);
+
+    const postTemplate = path.resolve(`./src/components/page.js`)
+    pages.edges.forEach(edge => {
+        createPage({
+            // will be the url for the page
+            path: edge.node.slug,
+            // specify the component template of your choice
+            component: slash(postTemplate),
+            // In the ^template's GraphQL query, 'id' will be available
+            // as a GraphQL variable to query for this posts's data.
+            context: {
+                id: edge.node.id,
+            },
+        })
+    });
+
+    // forward / to /home:
+    createRedirect({
+        fromPath: `/`,
+        toPath: `/miraculous-productions/`,
+        redirectInBrowser: true,
+        isPermanent: true,
+    });
+}
